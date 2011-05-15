937b0b43556fc4e9f3e3bc16ec60a4d6252d4e24
Dan Kalotay <d.kalotay@gmail.com>
2011-05-15 23:14:54 +0100

    added fitness measure

--- meiosis.py
+++ meiosis.py
@@ -5,6 +5,12 @@ import hammingdist as hd
 #all possible characters
 ALPHABET = ' '.join([string.lowercase, string.punctuation])
 
+def fitness(phrase, target):
+#fitness of a phrase
+#defined as the phrase length minus the Hamming distance to target
+    assert len(phrase) == len(target)
+    return len(phrase) - hd.hamming_distance(phrase, target)
+
 def mutate(phrase, rate):
     assert 0.0 <= rate <= 1.0
     for c in phrase:
